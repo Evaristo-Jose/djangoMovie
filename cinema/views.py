50f7f938000cd14e1733269ce5ac7a25976fdd58
Evaristo-Jose <neshrak@hotmail.com>
2021-06-04 22:15:14 +0200

    arreglos varios

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
+import io
 import os
 import requests
-import io
+from urllib.parse import urlparse
 from PIL import Image
 
 from django.core.exceptions import ObjectDoesNotExist
@@ -10,8 +11,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from .forms import MovieForm
 from .models import Movie
-
-from urllib.parse import urlparse
 
 
 def movie_view(request, id):
@@ -58,29 +57,42 @@ def movie_new(request):
     return render(request, 'movie/movie-detail.html', context=data, )
 
 
+def url_web(url):
+    global myimage
+    response = requests.get(url)
+    img = Image.open(io.BytesIO(response.content))
+    parsed = urlparse(url)
+    cadena = parsed.path
+    posicion_barra = cadena.rfind('/')
+    myimage = cadena[posicion_barra + 1:]
+    location = '/static/img/'
+    img.save(os.getcwd() + location + myimage)
+
+
 def movie_save(request):
-    global movie
+    global movie, img
     id_movie = request.POST.get('id')
     if id_movie == '':
         form = MovieForm(request.POST, request.POST.get('image_url'), request.FILES)
+        if request.POST.get('image_url') != '':
+            url = request.POST.get('image_url')
+            url_web(url)
+            form.instance.image = myimage
+            form = MovieForm(request.POST)
+        else:
+            form = MovieForm(request.POST, request.FILES)
     else:
         id_movie = int(id_movie)
         movie = Movie.objects.get(id=id_movie)
         form = MovieForm(request.POST, request.POST.get('image_url'), request.FILES, instance=movie)
         if request.POST.get('image_url') != '':
             url = request.POST.get('image_url')
-            response = requests.get(url)
-            img = Image.open(io.BytesIO(response.content))
-            parsed = urlparse(url)
-            cadena = parsed.path
-            posicion_barra = cadena.rfind('/')
-            myimage = cadena[posicion_barra + 1:]
-            location = '/static/img/'
-            img.save(os.getcwd() + location + myimage)
+            url_web(url)
             movie.image = myimage
+            form = MovieForm(request.POST, instance=movie)
         else:
+            form = MovieForm(request.POST, request.FILES, instance=movie)
             movie.image = request.FILES.get('image')
-    form = MovieForm(request.POST, request.FILES, instance=movie)
     if not form.is_valid():
         return HttpResponseRedirect('/cinema/movie-new')
     location = 'static/img'
